d84943756d9f3fbe708ceab8d55037f1036403ad
Amélie Cousineau <ameliecousineau5@gmail.com>
2025-10-11 11:03:15 -0400

    Merge branch 'main' of https://github.com/tim-a25-pw/pw_webjam-ctrl-s

--- theme/functions.php
+++ theme/functions.php
@@ -70,3 +70,174 @@ function book() {
 
 }
 add_action( 'init', 'book', 0 );
+
+// Register Custom Post Type
+function music() {
+
+	$labels = array(
+		'name'                  => _x( 'Musiques', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Musique', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Musique', 'text_domain' ),
+		'name_admin_bar'        => __( 'Musique', 'text_domain' ),
+		'archives'              => __( 'Archive de la musique', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter nouvelle musique', 'text_domain' ),
+		'add_new'               => __( 'Ajouter nouvelle musique', 'text_domain' ),
+		'new_item'              => __( 'Nouvelle musique', 'text_domain' ),
+		'edit_item'             => __( 'Modifier musique', 'text_domain' ),
+		'update_item'           => __( 'Mettre à jour musique', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Musique', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-audio',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'music', $args );
+
+}
+add_action( 'init', 'music', 0 );
+
+// Register Custom Post Type
+function movie() {
+
+	$labels = array(
+		'name'                  => _x( 'Films', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Film', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Film', 'text_domain' ),
+		'name_admin_bar'        => __( 'Films', 'text_domain' ),
+		'archives'              => __( 'Archive des films', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter nouveau film', 'text_domain' ),
+		'add_new'               => __( 'Ajouter nouveau film', 'text_domain' ),
+		'new_item'              => __( 'Nouveau film', 'text_domain' ),
+		'edit_item'             => __( 'Modifier film', 'text_domain' ),
+		'update_item'           => __( 'Mettre à jour film', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Film', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-format-video',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'movie', $args );
+
+}
+add_action( 'init', 'movie', 0 );
+
+// Register Custom Post Type
+function member() {
+
+	$labels = array(
+		'name'                  => _x( 'Membres', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Membre', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Membre', 'text_domain' ),
+		'name_admin_bar'        => __( 'Membres', 'text_domain' ),
+		'archives'              => __( 'Archive des membres', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Items', 'text_domain' ),
+		'add_new_item'          => __( 'Ajouter nouveau membre', 'text_domain' ),
+		'add_new'               => __( 'Ajouter nouveau membre', 'text_domain' ),
+		'new_item'              => __( 'Nouveau membre', 'text_domain' ),
+		'edit_item'             => __( 'Modifier membre', 'text_domain' ),
+		'update_item'           => __( 'Mettre à jour membre', 'text_domain' ),
+		'view_item'             => __( 'View Item', 'text_domain' ),
+		'view_items'            => __( 'View Items', 'text_domain' ),
+		'search_items'          => __( 'Search Item', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Membre', 'text_domain' ),
+		'description'           => __( 'Post Type Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-businessman',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'member', $args );
+
+}
+add_action( 'init', 'member', 0 );
