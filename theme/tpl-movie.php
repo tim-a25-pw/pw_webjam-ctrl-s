deb8fc7031b0b6c2827c2fbc32431d6c534845d5
Amélie Cousineau <ameliecousineau5@gmail.com>
2025-10-12 12:24:43 -0400

    tpl-movie dynamiser

---
+++ theme/tpl-movie.php
@@ -0,0 +1,178 @@
+    <?php /* Template Name: Univers Movie */ ?>
+
+<?php get_header(); ?>
+
+    <section class="hero-cinema">
+        <div class="wrapper">
+            <div class="hero__media">
+                <?php the_post_thumbnail(); ?>
+            </div>
+            <div class="hero__content">
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>            
+            </div>
+        </div>
+    </section>
+
+    <!-- OEUVRE VEDETTE -->
+    <?php 
+        $featured = array(
+            'post_type'=> 'movie',
+            'post_status'=> 'publish',
+            'posts_per_page'=> -1,
+            'meta_query'     => array(
+                array(
+                        'key'     => 'movie-featured',
+                        'value'   => '1',
+                        'compare' => '='
+                    )
+                )
+            );
+        $query_featured = new WP_Query( $featured );
+    ?>
+
+    <section class="oeuvres">
+        <div class="wrapper">
+            <h2 class="h2-music">Oeuvres</h2>
+            <?php if( $query_featured ->have_posts() ) : ?>
+                <?php while( $query_featured->have_posts()): $query_featured->the_post(); ?>
+                    <div class="vedette">
+                        <div class="content">
+                            <h3><?php the_title(); ?></h3>
+                            <?php the_excerpt(); ?>
+                        </div>
+                        <div class="media">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+            <?php wp_reset_query(); ?>
+
+            <!-- OEUVRE PAS VEDETTE -->
+            <?php 
+                $not_featured = array(
+                    'post_type'=> 'movie',
+                    'post_status'=> 'publish',
+                    'posts_per_page '=> -1,
+                    'meta_query'     => array(
+                        array(
+                            'key'     => 'movie-featured',
+                            'value'   => '0',
+                            'compare' => '='
+                        )
+                    )
+                );
+                $query_not_featured = new WP_Query( $not_featured );
+            ?> 
+
+            <?php if( $query_not_featured ->have_posts() ) : ?>
+            <div class="cards swiper swiper-lecture green" data-component="Carousel" data-split data-direction="vertical" data-slides="2" data-space="55">
+                <div class="custom-pagination"></div>
+                <div class="swiper-wrapper">
+                    <?php while( $query_not_featured->have_posts()): $query_not_featured->the_post(); ?>
+                        <a href="<?php the_permalink(); ?>" class="card swiper-slide">
+                            <div class="card__media">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="card__content">
+                                <h3><?php the_title(); ?></h3>
+                                <?php the_excerpt(); ?>                            
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+            <?php endif; ?>
+            <?php wp_reset_query(); ?>
+            <a href="<?php bloginfo('url') ?>/soumettre-une-oeuvre" class="btn green">Soumettre une oeuvre</a>
+        </div>
+    </section>
+
+    <section class="interaction">
+        <div class="wrapper">
+            <div class="media">
+                <img src="<?php bloginfo('template_url') ?>/assets/images/lunetteInteractionCinema.png" alt="">
+            </div>
+            <div class="content">
+                <h2 class="h2-music">Interaction</h2>
+                <p>Les grands classiques du cinéma reprennent vie devant vos yeux. Grâce à un affichage immersif intégré, Vision vous permet de redécouvrir des chefs d’œuvre  comme si l’écran vous enveloppait tout entier, à l’abri de toute censure.</p>
+                <ul>
+                    <li><span class="green">Abaissez les lunettes</span> pour lancer la projection.</li>
+                    <li><span class="green">Faites glisser votre regard de droite à gauche</span> pour changer de scène, et laissez le film se dérouler autour de vous, entre rêve et réalité.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section class="souvenir">
+        <div class="wrapper">
+            <h2 class="h2-music">Des noms à se souvenir</h2>
+            <div class="grid-3">
+                <div class="card-souvenir card-souvenir_lecture">
+                    <div class="card__media">
+                        <img src="assets/images/charlieChaplin.png" alt="">
+                    </div>
+                    <div class="card__content">
+                        <h3>Charlie Chaplin</h3>
+                    </div>
+                </div>
+                <div class="card-souvenir card-souvenir_lecture">
+                    <div class="card__media">
+                        <img src="assets/images/alfredHitchcock.png" alt="">
+                    </div>
+                    <div class="card__content">
+                        <h3>Alfred Hitchcock</h3>
+                    </div>
+                </div>
+                <div class="card-souvenir card-souvenir_lecture">
+                    <div class="card__media">
+                        <img src="assets/images/jeanRenoir.png" alt="">
+                    </div>
+                    <div class="card__content">
+                        <h3>Jean Renoir</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php 
+        $favorites = array(
+            'post_type'=> 'music',
+            'post_status'=> 'publish',
+            'posts_per_page '=> -1,
+            'meta_query'     => array(
+                array(
+                    'key'     => 'music-favorite',
+                    'value'   => '1',
+                    'compare' => '='
+                )
+            )
+        );
+        $query_favorites = new WP_Query( $favorites );
+    ?> 
+
+    <?php if( $query_favorites ->have_posts() ) : ?>
+        <section class="hits">
+            <div class="wrapper">
+                <h2 class="h2-music">Les meilleurs hits</h2>
+                <div class="grid-3">
+                    <?php while( $query_favorites->have_posts()): $query_favorites->the_post(); ?>
+                        <a href="<?php the_permalink(); ?>" class="card-hits card-hits_lecture">
+                            <div class="card__media">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <div class="card__content">
+                                <h3><?php the_title(); ?></h3>
+                                <?php the_excerpt(); ?>
+                            </div>
+                        </a>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+        </section>
+    <?php endif; ?>
+    <?php wp_reset_query(); ?>
+            
+<?php get_footer(); ?>
